f74f90435d655c4b3ab537b271834f0b7e0e2110
Nikhil <nikhilchobhiyal009@gmail.com>
2024-03-18 18:50:33 +0530

    added a new package that print text below the joke

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ var Joke=require(
     'One-liner-joke'
 );
 // This is a line below to print the joke 
-console.log(Joke.getRandomJoke());
+var hehe=Joke.getRandomJoke();
+console.log(hehe);
+
+
+var figlet=require("figlet");
+figlet("Nice Joke", function (err, data){
+    if(err)
+    {
+        console.log("Something went wrong..");
+        console.log(err);
+        return;
+    }
+    console.log(data);
+});
